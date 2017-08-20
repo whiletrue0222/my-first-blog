60ee616cde961f230eb9a59c876445481dd451d1
youngkwon lee <whiletrue0222@gmail.com>
2017-08-20 15:00:13 +0900

    a
    a

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko'
 
 # TIME_ZONE = 'UTC'
 TIME_ZONE = 'Asia/Seoul'
